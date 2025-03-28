46065b46312173a3307ea15c04efdbd81dd0f38a
SarahVenancio <sarah.venancio.senaisp@gmail.com>
2025-03-28 10:49:39 -0300

    importações

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
 import random
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 charadas = [
     {'id': 1, 'charada': 'A Dora é mestre em artes marciais. Sabe qual é o seu golpe preferido?', 'resposta': 'A voa-Dora'},
@@ -42,4 +44,4 @@ def busca(campo, busca):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
